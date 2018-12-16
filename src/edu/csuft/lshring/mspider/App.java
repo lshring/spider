bf8ad0bc86c5690753d2c487d6365daa1d6d196a
lshring <45841573+lshring@users.noreply.github.com>
2018-12-16 21:11:37 +0800

    解析影片信息

---
+++ src/edu/csuft/lshring/mspider/App.java
@@ -0,0 +1,59 @@
+package edu.csuft.lshring.mspider;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * 
+ * @author wtao
+ *
+ */
+public class App {
+
+	// alt + /
+	// ctrl + s
+	public static void main(String[] args) {
+
+		// 目标 URL
+		String url = "https://movie.douban.com/top250";
+
+		// 使用 JSOUP 抓去文档
+		try {
+			Document doc = Jsoup.connect(url).get();
+
+			Elements es = doc.select(".grid_view .item");
+			System.out.println(es.size());
+
+			// 创建一个影片的列表
+			ArrayList<Film> list = new ArrayList<>();
+
+			for (Element e : es) {
+				Film f = new Film();
+				// 每一部影片
+				Element t = e.select(".title").first();
+				String num = e.select(".star span").last().text();
+				System.out.println(t.text() + ", " + num);
+
+//				f.id
+//				f.title
+				list.add(f);
+			}
+
+//			String title = doc.title();
+//			String data = doc.data();
+
+//			System.out.println(title);
+//			System.out.println(data);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/edu/csuft/lshring/mspider/Film.java
@@ -0,0 +1,46 @@
+package edu.csuft.lshring.mspider;
+
+/**
+ * 影片
+ * 
+ * @author wtao
+ *
+ */
+public class Film {
+
+	/**
+	 * 影片名称
+	 */
+	String title;
+
+	/**
+	 * 相关信息
+	 */
+	String info;
+
+	/**
+	 * 评分
+	 */
+	double rating;
+
+	/**
+	 * 评分人数
+	 */
+	int num;
+
+	/**
+	 * 排名
+	 */
+	int id;
+
+	/**
+	 * 海报
+	 */
+	String poster;
+
+	/**
+	 * 短评
+	 */
+	String quote;
+
+}
